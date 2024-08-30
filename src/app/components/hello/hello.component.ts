ba652c957b96e95e108f52a68a35bccf50b2f651
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-30 13:21:04 +0200

    swear this will be the last cartouselm ill buiuodl

---
+++ src/app/components/hello/hello.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { startWith, take, map } from 'rxjs/operators';
+import { NguCarouselConfig } from '@ngu/carousel';
+import { slider } from './hello-slide.animation'
+
+@Component({
+  selector: 'hello',
+  templateUrl: 'hello.component.html',
+  styleUrls: ['hello.component.scss'],
+  animations: [slider],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelloComponent implements OnInit {
+  @Input() name: string;
+  imgags = [
+    'assets/bg.jpg',
+    'assets/car.png',
+    'assets/canberra.jpg',
+    'assets/holi.jpg'
+  ];
+  public carouselTileItems$: Observable<number[]>;
+  public carouselTileConfig: NguCarouselConfig = {
+    grid: { xs: 1, sm: 1, md: 1, lg: 5, all: 0 },
+    speed: 250,
+    point: {
+      visible: true
+    },
+    touch: true,
+    loop: true,
+    interval: { timing: 1500 },
+    animation: 'lazy'
+  };
+  tempData: any[];
+  
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.tempData = [];
+    this.carouselTileItems$ = interval(500).pipe(
+      startWith(-1),
+      take(30),
+      map(val => {
+        const data = (this.tempData = [
+          ...this.tempData,
+          this.imgags[Math.floor(Math.random() * this.imgags.length)]
+        ]);
+        return data;
+      })
+    );
+  }
+
+}
